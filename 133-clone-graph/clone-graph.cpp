9a47f4e4c8e34a49e05632246541122c1ae6f103
Mayank Agrawal <mayankagrawal.27.2005@gmail.com>
2026-07-02 17:51:55 +0530

    Time: 3 ms (68.35%) | Memory: 12.2 MB (28.12%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* dfs(Node* node, unordered_map<Node*, Node*>&mp){
+        if (mp.count(node)){
+            return mp[node];
+        }
+        Node* newNode = new Node(node->val);
+        mp[node] = newNode;
+        for (auto it: node->neighbors){
+            newNode->neighbors.push_back(dfs(it, mp));
+        }
+        return newNode;
+    }
+
+    Node* cloneGraph(Node* node) {
+        if (!node) return NULL;
+        unordered_map<Node*, Node*>mp;
+        return dfs(node, mp);
+    }
+};
