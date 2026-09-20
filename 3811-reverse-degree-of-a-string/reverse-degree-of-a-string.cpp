1d05da72c70da8c6c2b770223b6c7fa41fc340db
Mayank Agrawal <mayankagrawal.27.2005@gmail.com>
2026-09-20 10:02:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -5,7 +5,6 @@ public:
         for (int i=0; i<s.size(); i++){
             int reversed = 'z' - s[i] + 1;
             res += reversed * (i+1);
-            cout << reversed * (i+1) << endl;
         }
         return res;
     }
